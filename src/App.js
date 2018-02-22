f61af45cf0344aa29f41278d932349f2ef43d622
Neelam shah <neelam@L-156152672.local>
2018-02-22 18:56:46 +0000

    adding first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+    
+  }
+  
+
+
+
+  render() {
+    
+    return (
+      <div className="App">
+       <div className = "App-header"> Welcome to Digital Hackathon London </div>
+      </div>
+    );
+  }
+}
+
+export default App;
