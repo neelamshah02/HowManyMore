f36b69711fc182ff63e5f4f3037a732b690c1f62
gnanma <gnan.madishetty@wipro.com>
2018-02-23 13:03:15 +0000

    DotChart - added axes
    and some colour changes

--- src/sketch.js
+++ src/sketch.js
@@ -3,6 +3,8 @@ export default function sketch (p) {
 var shootings; //JSON data of shootings
 var width;
 var height;
+var minYear;
+var maxYear;
 var isPreLoadDone;
 var incidents = [];
 var trans;
@@ -29,6 +31,9 @@ p.setup = function (){
   width = document.getElementById('chartShootings').offsetWidth-20;
   height = document.getElementById('chartShootings').offsetHeight-20;
   trans = 10;
+  minYear = 1966;
+  maxYear = 2016;
+
 
   var canvas = p.createCanvas(width, height);
   canvas.parent("chartShootings");
@@ -38,6 +43,7 @@ p.setup = function (){
   p.noStroke();
   p.fill('rgba(255, 255, 255,1)');
   p.rect(0,0,width-2,height+2,10,10);
+  p.drawAxes();
 
   for(var i=0;i<shootings.records.length;i++){
 
@@ -46,16 +52,85 @@ p.setup = function (){
   //console.log(shootings.records[i].fields.date);
   //console.log(shootings.records[i].fields.number_of_victims_injured);
   var dayOfYear = ((p.int(shootings.records[i].fields.date.substring(5,7))-1)*30)+p.int((shootings.records[i].fields.date.substring(8,10)));
-  console.log(dayOfYear);
+  //console.log(dayOfYear);
   incidents.push({dayOfYear: dayOfYear,
                   month:shootings.records[i].fields.date.substring(5,7),
                   year:shootings.records[i].fields.date.substring(0,4),
                   fatalities:shootings.records[i].fields.number_of_victim_fatalities,
-                  injured:shootings.records[i].fields.number_of_victims_injured});
+                  injured:shootings.records[i].fields.number_of_victims_injured,
+                  link:shootings.records[i].fields.data_source_1});
   }
+  var button;
+
+  //button = p.ellipse(100,100,100,100);
+  //button.mouseOver(console.log(1));
+
 
 }
 
+p.mouseClicked = function() {
+
+ if (p.mouseX >100 && p.mouseY >100)
+ console.log(maxYear);
+//  document.out("abcd")
+
+}
+
+
+p.website = function (){
+    p.link("p5js.org");
+}
+
+p.link = function (href, target){
+  if (target !== 'undef')  p.window.open(href, target);
+  else                   p.window.location = href;
+};
+
+
+p.drawAxes = function (){
+
+  var x;
+  var y;
+
+  p.fill(110, 10, 253, 100);
+  p.textSize(10);
+  //console.log(maxYear);
+
+  for(var i=maxYear;i>minYear-1;i--){
+    y = p.map(i,minYear,maxYear,height-10,20);
+    p.text(i, 5, y);
+    p.text(i, width-25, y);
+
+    //console.log(i);
+  }
+
+
+  for(var i=0;i<12;i++){
+    x = p.map(i,0,11,20,width-50);
+    //console.log(p.getMonth(i))
+    p.text(p.getMonth(i), x, 10);
+    p.text(p.getMonth(i), x, height-10);
+
+    //console.log(i);
+  }
+
+
+}
+
+
+
+
+p.getMonth = function(monthNumber) {
+  var monthNames = [
+    "Jan", "Feb", "Mar",
+    "Apr", "May", "Jun", "Jul",
+    "Aug", "Sep", "Oct",
+    "Nov", "Dec"
+  ];
+  var monthIndex = monthNumber;
+
+  return monthNames[monthIndex];
+}
 
 
 p.draw = function (){
@@ -76,13 +151,14 @@ p.draw = function (){
       p.noStroke();
 
     for(var i=0;i<incidents.length;i++){
-       x = p.map(incidents[i].dayOfYear,1,365,20,width-20); //min 1966-08-01 max2016-02-09
-       y = p.map(incidents[i].year,1966,2016,height-20,20);
+       x = p.map(incidents[i].dayOfYear,1,365,20,width-20); //min 1966-08-01 max 2016-02-09
+       y = p.map(incidents[i].year,minYear,maxYear,height-20,20);
        r = p.map(incidents[i].fatalities,0,32,10,25);  // min 0 max 32
-       strokeWeight = p.map(incidents[i].injured,0,32,2,10);  // min 0 max 32
-       p.stroke(strokeWeight  );
-       p.stroke('rgba(0 ,255,255,0.8)')
+       strokeWeight = p.map(incidents[i].injured,0,32,5,15);  // min 0 max 32
+       p.stroke(strokeWeight);
+       p.stroke('rgba(242, 213, 24,0.8)')
        p.ellipse(x, y, r, r);
+       //p.document.out(incidents[i].fatalities.link)
        //console.log("x = "+x);
        //console.log("y = "+y);
 
